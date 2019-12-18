97be5e5511dabba35524f254f7a19a7c2836a86e
MIRXU <172307008@qq.com>
2019-12-18 11:48:35 +0800

    Update mv.py

--- scamps/mv.py
+++ scamps/mv.py
@@ -1,15 +1,15 @@
 #coding=utf-8
-import urllib,urllib2
+from urllib import request
+from urllib.request import urlretrieve
 import re
 def getUrlList(page):
-    req=urllib2.Request('http://www.budejie.com/video/%d' %page)
+    req=request.urlopen('http://www.budejie.com/video/%d' %page)
     req.add_header('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36')
-    res=urllib2.urlopen(req)
     html=res.read()
     res=r'data-mp4="(.*?)">'
     urllist=re.findall(res,html)
     for url in urllist:
-        urllib.urlretrieve(url,'mp4/%s.mp4' %url.split('/')[-1])
+        urlretrieve(url,'mp4/%s.mp4' %url.split('/')[-1])
 
-for page in xrange(1000):
+for page in range(1000):
     getUrlList(page+1)
